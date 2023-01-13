fcae304a50d65c2e93fca84b4da6c9537c19beec
Pouya Kharazi <pouyakharazi@gmail.com>
2023-01-13 17:04:21 +0330

    new folder

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,16 +1,25 @@
- let backdrop = document.querySelector('.backdrop')
+let backdrop = document.querySelector(".backdrop");
 
 //  console.log(backdrop)
 // backdrop.style.display = 'block'
 
- let modal = document.querySelector('.modal')
+let modal = document.querySelector(".modal");
 
-let selectPlanBtn = document.querySelectorAll('.plan button')
+let selectPlanBtn = document.querySelectorAll(".plan button");
 
+let modalClousBtn = document.querySelector(".modal__action--negative");
 
-for (let i = 0; i < selectPlanBtn.length ; i++) {
-      selectPlanBtn[i].addEventListener('click', () => {
-      modal.style.display = 'block'
-      backdrop.style.display = 'block'
-      })
+for (let i = 0; i < selectPlanBtn.length; i++) {
+  selectPlanBtn[i].addEventListener("click", () => {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener('click', closeModal);
+modalClousBtn.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
 }
